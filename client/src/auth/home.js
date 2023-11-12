dd6d54cc8c08afa59f29c2135bb5211ca7cb3abc
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-12 06:36:28 -0500

    Update home.js

--- client/src/auth/home.js
+++ client/src/auth/home.js
@@ -3,10 +3,12 @@ import './home.css';
 
 const AuthHome = () => {
   return (
-    <div className="main-content">
-      <h1 className="title">PortionPredictor</h1>
-      <p className="slogan">Never waste food again.</p>
-      <a href="./register" className="start-for-free-center">Start for Free</a>
+    <div className="home-container">
+      <div className="main-content">
+        <h1 className="title">PortionPredictor</h1>
+        <p className="slogan">Never waste food again.</p>
+        <a href="./register" className="start-for-free-center">Start for Free</a>
+      </div>
     </div>
   );
 };
