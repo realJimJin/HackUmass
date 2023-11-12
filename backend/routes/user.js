07d151f9ad60ca855ebe326ef0f22cdddee9a5d6
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-12 06:34:56 -0500

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 // controller functions 
-const { signupUser, loginUser } = require('../controllers/userController')
+const { signupUser, loginUser, updateUser } = require('../controllers/userController')
 
 const router = express.Router()
 
@@ -10,6 +10,9 @@ router.post('/login', loginUser)
 
 // signup route
 router.post('/signup', signupUser)
+
+// userRoutes.js
+router.patch('/', updateUser);
 
 
 module.exports = router
