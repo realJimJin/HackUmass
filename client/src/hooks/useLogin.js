698de2a51fa8ba300ae8280ce9a2acbacadf366a
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-12 06:37:35 -0500

    Update useLogin.js

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -1,38 +1,40 @@
-import { useState } from 'react'
-import { useAuthContext } from './useAuthContext'
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
 
 export const useLogin = () => {
-    const [error, setError] = useState(null)
-    const [isLoading, setIsLoading] = useState(null)
-    const { dispatch } = useAuthContext()
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false); // Initialize as false
+    const { dispatch } = useAuthContext();
 
     const login = async (email, password) => {
         setIsLoading(true);
         setError(null);
-    
-        const response = await fetch('https://hackumass.onrender.com/api/user/login', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({email, password})
-        });
-    
-        // Use await here to get the resolved value from the promise
-        const json = await response.json();
-    
-        if (!response.ok) {
-            setIsLoading(false)
-            setError(json.error)
-            return;  // Make sure to return to stop further execution
-        }
-    
-        // save user to local storage
-        localStorage.setItem('user', JSON.stringify(json));
-    
-        // update auth context
-        dispatch({type: 'LOGIN', payload: json});
-    
-        setIsLoading(false);
-    }
 
-    return { login, isLoading, error}
-}
+        try {
+            const response = await fetch('https://hackumass.onrender.com/api/user/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password })
+            });
+
+            const json = await response.json();
+
+            if (!response.ok) {
+                setIsLoading(false);
+                setError(json.error || 'An error occurred during login'); // Fallback error message
+            }
+
+            if (response.ok) {
+                // Save user details to local storage, including _id
+                localStorage.setItem('user', JSON.stringify(json));
+                dispatch({ type: 'LOGIN', payload: json });
+                setIsLoading(false);
+            }
+        } catch (err) {
+            setIsLoading(false);
+            setError(err.message || 'Failed to log in');
+        }
+    };
+
+    return { login, isLoading, error };
+};
