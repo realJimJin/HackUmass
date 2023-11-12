3895bb80e28196fa501f0122777a75af50a2f5e6
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2023-11-12 02:54:08 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,45 @@ const userRoutes = require('./routes/user');
 // Express app
 const app = express();
 
+// Allowed origins for CORS
+const allowedOrigins = [
+    'http://localhost:3000', // Your local development domain
+    'https://spectacular-dolphin-335f97.netlify.app', // Netlify domain
+    // Add other domains you want to whitelist here
+];
+
+// CORS configuration
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true); // Allow requests with no origin (like mobile apps, curl, etc.)
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true, // Allow sending credentials like cookies and HTTP authentication
+}));
+
 // Middleware for parsing JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.path}, Body:`, req.body);
+    next();
+});
 
 // Routes
 app.use('/api/user', userRoutes);
 
-// Start the server
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-});
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('MongoDB Connection Error:', error);
+    });
